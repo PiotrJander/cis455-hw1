5bd851740c92794015368b86a4fa281e8277c45a
PiotrJander <pjander2@gmail.com>
2016-09-28 17:12:47 -0400

    Added HTTP statuses

--- src/edu/upenn/cis/cis455/webserver/HttpStatus.java
+++ src/edu/upenn/cis/cis455/webserver/HttpStatus.java
@@ -3,8 +3,10 @@ package edu.upenn.cis.cis455.webserver;
 enum HttpStatus {
     CONTINUE("100", "Continue"),
     OK("200", "OK"),
+    NOT_MODIFIED("304", "Not Modified"),
     BAD_REQUEST("400", "Bad Request"),
     NOT_FOUND("404", "Not Found"),
+    PRECONDITION_FAILED("412", "Precondition Failed"),
     INTERNAL_SERVER_ERROR("500", "Internal Server Error"),
     NOT_IMPLEMENTED("501", "Not Implemented");
 
