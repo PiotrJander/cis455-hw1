2d717c623b7cb62dbd042b148e73d76ec6a77ec6
PiotrJander <pjander2@gmail.com>
2016-09-28 20:00:03 -0400

    Run ant builds!

--- test/edu/upenn/cis/cis455/webserver/TaskTest.java
+++ test/edu/upenn/cis/cis455/webserver/TaskTest.java
@@ -2,8 +2,6 @@ package edu.upenn.cis.cis455.webserver;
 
 import junit.framework.TestCase;
 
-import static org.junit.Assert.*;
-
 public class TaskTest extends TestCase {
 
     public void testMime() throws Exception {
