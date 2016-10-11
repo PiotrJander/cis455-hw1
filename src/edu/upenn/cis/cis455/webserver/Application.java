ef626b25ae2479cafb44d96b3e91a5cf07cb0d62
PiotrJander <pjander2@gmail.com>
2016-10-10 22:38:40 -0400

    Handles invalid web.xml gracefully

--- src/edu/upenn/cis/cis455/webserver/Application.java
+++ src/edu/upenn/cis/cis455/webserver/Application.java
@@ -2,6 +2,7 @@ package edu.upenn.cis.cis455.webserver;
 
 import edu.upenn.cis.cis455.webserver.servlet.ServletConfig;
 import edu.upenn.cis.cis455.webserver.servlet.ServletContext;
+import org.w3c.dom.DOMException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
@@ -16,27 +17,35 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 
-public class Application {
+class Application {
 
     private File webDotXmlSource;
     private Document webDotXml;
     private ServletContext servletContext;
     private HashMap<String, HttpServlet> servletsNameToClassMapping = new HashMap<>();
+    private HashMap<String, String> urlMapping = new HashMap<>();
 
     Application(File webDotXmlSource) {
         this.webDotXmlSource = webDotXmlSource;
     }
 
     void create() {
-        parseWebDotXml();
-        makeServletContext();
-        loadServlets();
+        try {
+            parseWebDotXml();
+            makeServletContext();
+            loadServlets();
+            loadUrlMapping();
+        } catch (WebDotXmlException e) {
+            System.out.println("Invalid web.xml");
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     /**
      * Parses web.xml, and saves the DOM as `webDotXml`.
      */
-    private void parseWebDotXml() {
+    private void parseWebDotXml() throws WebDotXmlException {
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
@@ -44,26 +53,28 @@ public class Application {
         } catch (ParserConfigurationException | IOException e) {
             e.printStackTrace();
         } catch (SAXException e) {
-            System.out.println("Invalid XML in web.xml.");
-            HttpServer.handleInvalidArguments();
+            throw new WebDotXmlException(e);
         }
     }
 
     /**
      * Creates a ServletContext from data in web.xml.
      */
-    private void makeServletContext() {
-        String displayName = webDotXml.getElementsByTagName("display-name").item(0).getTextContent();
-
-        NodeList contextParamsXml = webDotXml.getElementsByTagName("context-param");
-        HashMap<String, String> contextInitParams = new HashMap<>();
-        for (int i = 0; i < contextParamsXml.getLength(); i++) {
-            Element param = (Element) contextParamsXml.item(i);
-            String name = param.getElementsByTagName("param-name").item(0).getTextContent();
-            String value = param.getElementsByTagName("param-value").item(0).getTextContent();
-            contextInitParams.put(name, value);
+    private void makeServletContext() throws WebDotXmlException {
+        try {
+            String displayName = webDotXml.getElementsByTagName("display-name").item(0).getTextContent();
+            NodeList contextParamsXml = webDotXml.getElementsByTagName("context-param");
+            HashMap<String, String> contextInitParams = new HashMap<>();
+            for (int i = 0; i < contextParamsXml.getLength(); i++) {
+                Element param = (Element) contextParamsXml.item(i);
+                String name = param.getElementsByTagName("param-name").item(0).getTextContent();
+                String value = param.getElementsByTagName("param-value").item(0).getTextContent();
+                contextInitParams.put(name, value);
+            }
+            servletContext = new ServletContext(displayName, contextInitParams);
+        } catch (ClassCastException | NullPointerException e) {
+            throw new WebDotXmlException(e);
         }
-        servletContext = new ServletContext(displayName, contextInitParams);
     }
 
     /**
@@ -73,36 +84,71 @@ public class Application {
      * 4. Creates a ServletConfig from <init-param> elements.
      * 5. Calls the servlet's `init` method.
      */
-    private void loadServlets() {
+    private void loadServlets() throws WebDotXmlException {
         NodeList servlets = webDotXml.getElementsByTagName("servlet");
         for (int i = 0; i < servlets.getLength(); i++) {
-            Element servletElement = (Element) servlets.item(i);
-            String servletName = servletElement.getElementsByTagName("servlet-name").item(0).getTextContent();
-            String className = servletElement.getElementsByTagName("servlet-class").item(0).getTextContent();
+            Element servletElement;
+            String servletName;
+            String className;
+            try {
+                servletElement = (Element) servlets.item(i);
+                servletName = servletElement.getElementsByTagName("servlet-name").item(0).getTextContent();
+                className = servletElement.getElementsByTagName("servlet-class").item(0).getTextContent();
+            } catch (ClassCastException | NullPointerException e) {
+                throw new WebDotXmlException(e);
+            }
 
             HttpServlet servlet;
             try {
                 servlet = (HttpServlet) Class.forName(className).newInstance();
             } catch (InstantiationException | IllegalAccessException | ClassNotFoundException e) {
-                e.printStackTrace();
-                continue;
+                throw new WebDotXmlException("Invalid class for the servlet.", e);
             }
 
             servletsNameToClassMapping.put(servletName, servlet);
 
-            NodeList initParamElementSet = servletElement.getElementsByTagName("init-param");
-            HashMap<String, String> initParameters = new HashMap<>();
-            for (int j = 0; j < initParamElementSet.getLength(); j++) {
-                Element initParamElement = (Element) initParamElementSet.item(j);
-                String paramName = initParamElement.getElementsByTagName("param-name").item(0).getTextContent();
-                String paramValue = initParamElement.getElementsByTagName("param-value").item(0).getTextContent();
-                initParameters.put(paramName, paramValue);
+            HashMap<String, String> initParameters;
+            try {
+                NodeList initParamElementSet = servletElement.getElementsByTagName("init-param");
+                initParameters = new HashMap<>();
+                for (int j = 0; j < initParamElementSet.getLength(); j++) {
+                    Element initParamElement = (Element) initParamElementSet.item(j);
+                    String paramName = initParamElement.getElementsByTagName("param-name").item(0).getTextContent();
+                    String paramValue = initParamElement.getElementsByTagName("param-value").item(0).getTextContent();
+                    initParameters.put(paramName, paramValue);
+                }
+            } catch (ClassCastException | NullPointerException e) {
+                throw new WebDotXmlException(e);
             }
 
             try {
                 servlet.init(new ServletConfig(servletName, initParameters, servletContext));
             } catch (ServletException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * TODO url as regex rather than as string?
+     */
+    private void loadUrlMapping() throws WebDotXmlException {
+        NodeList servletMappingSet = webDotXml.getElementsByTagName("servlet-mapping");
+        for (int i = 0; i < servletMappingSet.getLength(); i++) {
+            String servletName;
+            String urlPattern;
+            try {
+                Element servletMapping = (Element) servletMappingSet.item(i);
+                servletName = servletMapping.getElementsByTagName("servlet-name").item(0).getTextContent();
+                urlPattern = servletMapping.getElementsByTagName("url-pattern").item(0).getTextContent();
+            } catch (ClassCastException | NullPointerException e) {
+                throw new WebDotXmlException(e);
+            }
+
+            if (servletsNameToClassMapping.containsKey(servletName)) {
+                urlMapping.put(servletName, urlPattern);
+            } else {
+                throw new WebDotXmlException("No servlet for the servlet name in mapping.");
             }
         }
     }
@@ -114,4 +160,11 @@ public class Application {
     HttpServlet getServletByName(String s) {
         return servletsNameToClassMapping.get(s);
     }
+}
+
+class WebDotXmlException extends Exception {
+    WebDotXmlException() { super(); }
+    WebDotXmlException(String message) { super(message); }
+    WebDotXmlException(String message, Throwable cause) { super(message, cause); }
+    WebDotXmlException(Throwable cause) { super(cause); }
 }
